1fb19ff22f2ee870481c3314d6449710c5da3594
Alexander Trojnin <trojninalex@usb-live>
2021-07-20 20:12:05 +0300

    Глава 2. Начало

---
+++ Глава 2/02.py
@@ -0,0 +1,4 @@
+message = "I go home."
+print(message)
+message = "I'm in my home."
+print(message)
---
+++ Глава 2/03.py
@@ -0,0 +1,2 @@
+name = "eric"
+print(f"Hello {name.title()}, would you like to learn some Python today?")
